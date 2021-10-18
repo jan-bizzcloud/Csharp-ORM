98c44ad78d3a94b187e1c2f2caaf588b91507e6c
jan-bizzcloud <jan@bizzcloud.com>
2021-10-18 14:10:46 +0200

    added recordset

--- Models/Model.cs
+++ Models/Model.cs
@@ -1,9 +1,16 @@
 using System;
+using Csharp_ORM.Fields;
 
 namespace Csharp_ORM.Models
 {
-    public class Model
+    public abstract class Model
     {
-        
+        public string name;
+        public string description;
+        public int id;
+        public abstract RecordSet create();
+        public abstract RecordSet search();
+        public abstract void update();
+        public abstract void delete();
     }
 }
---
+++ Models/RecordSet.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+
+namespace Csharp_ORM.Models
+{
+    public class RecordSet
+    {
+        public List<Model> records;
+    }
+}
